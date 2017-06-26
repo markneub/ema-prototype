323e7b78783284f4d84e1b31527e65fb873a1542
Mark Neuburger <markneub@gmail.com>
2017-06-26 13:47:59 -0700

    Remove template junk and enable SCSS in components

---
+++ src/components/EntryDashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="entry-dashboard">
+    howdy
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import EntryDashboard from '@/components/EntryDashboard'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'EntryDashboard',
+      component: EntryDashboard
     }
   ]
 })
