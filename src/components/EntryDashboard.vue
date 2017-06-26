e8fcabb8e0b878fb6b6d1dc8366eb88b2b31a248
Mark Neuburger <markneub@gmail.com>
2017-06-26 16:08:49 -0700

    Stub rating component

--- src/components/EntryDashboard.vue
+++ src/components/EntryDashboard.vue
@@ -2,51 +2,72 @@
   <div id="entry-dashboard">
     <div class="row">
       <div class="period morning">
-        <div class="name">Morning</div>
-        <div class="description">before noon</div>
+        <div class="pad">
+          <div class="name">Morning</div>
+          <div class="description">before noon</div>
+        </div>
       </div>
+      <rating v-for="measure in ['mood', 'stress', 'energy']" :measure="measure"></rating>
     </div>
     <div class="row">
       <div class="period afternoon">
-        <div class="name">Afternoon</div>
-        <div class="description">noon to 5:00</div>
+        <div class="pad">
+          <div class="name">Afternoon</div>
+          <div class="description">noon to 5:00</div>
+        </div>
       </div>
+      <rating v-for="measure in ['mood', 'stress', 'energy']" :measure="measure"></rating>
     </div>
     <div class="row last">
       <div class="period evening">
-        <div class="name">Evening</div>
-        <div class="description">5:00 to bedtime</div>
+        <div class="pad">
+          <div class="name">Evening</div>
+          <div class="description">5:00 to bedtime</div>
+        </div>
       </div>
+      <rating v-for="measure in ['mood', 'stress', 'energy']" :measure="measure"></rating>
     </div>
   </div>
 </template>
 
 <script>
+import Rating from '@/components/Rating'
+
 export default {
-  name: 'hello',
-  data () {
-    return {
-    }
+  components: {
+    Rating
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../scss/common.scss';
+
+#entry-dashboard {
+  min-width: 916px;
+  max-width: 1280px;
+  margin: 0 auto;
+}
+
 .row {
-  border-bottom: 1px solid rgba(146, 160, 182, 0.2);
+  display: flex;
+  border-bottom: $grid_style;
   &.last { border: none; }
-  padding: 24px 0;
+
   .period {
+    text-align: right;
+    width: 240px;
+    > .pad {
+      padding: 24px;
+    }
     &.morning {
-      padding-top: 36px;
-      background: url('../images/sunrise.png') top right/24px 16px no-repeat;
+      > .pad { padding-top: 48px; }
+      background: url('../images/sunrise.png') 80% top/24px 16px no-repeat;
     }
     &.evening {
-      padding-bottom: 36px;
-      background: url('../images/sunset.png') bottom right/22px 19px no-repeat;
+      > .pad { padding-bottom: 48px; }
+      background: url('../images/sunset.png') 80% bottom/22px 19px no-repeat;
     }
-    text-align: right;
-    width: 200px;
     .name {
       color: #4A4A4A;
       font-size: 30px;
@@ -56,6 +77,10 @@ export default {
       color: #92A0B6;
       font-size: 20px;
     }
+  }
+
+  .rating {
+    width: calc((100% - 240px) / 3);
   }
 }
 </style>
---
+++ src/components/Rating.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="['rating', measure]">
+    <div class="inner">
+      <button class="rate" disabled>Rate</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rating',
+  props: ['measure'],
+  data () {
+    return {
+      rating: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/common.scss';
+
+.rating {
+  border-left: $grid_style;
+
+  .inner {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  button.rate {
+    border-width: 1px;
+    border-style: solid;
+    background: transparent;
+    padding: 6px 8px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    text-transform: uppercase;
+    font-weight: 400;
+  }
+
+  &.mood button.rate {
+    color: $mood;
+    border-color: $mood;
+  }
+
+  &.stress button.rate {
+    color: $stress;
+    border-color: $stress;
+  }
+  &.energy button.rate {
+    color: $energy;
+    border-color: $energy;
+  }
+}
+</style>
